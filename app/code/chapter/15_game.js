1e5a80590a4e91c77b6083aba6401453205046ff
LonginusL33T <15112643615@163.com>
2017-06-03 17:54:14 +0800

    2017.6.3

--- app/code/chapter/15_game.js
+++ app/code/chapter/15_game.js
@@ -454,5 +454,56 @@ function range(start, end, step) {
     return stepArray;
 }
 
+function readStreamAsString(stream, callback) {
+    var content = "";
+    stream.on("data", function(chunk) {
+        content += chunk;
+    });
+    stream.on("end", function() {
+        callback(null, content);
+    });
+    stream.on("error", function(error) {
+        callback(error);
+    });
+}
+
+["text/plain", "text/html", "application/json"].forEach(function(type) {
+    http.request({
+        hostname: "eloquentjavascript.net",
+        path: "/author",
+        headers: {Accept: type}
+    }, function(response) {
+        if (response.statusCode != 200) {
+            console.error("Request for " + type + " failed: " + response.statusMessage);
+            return;
+        }
+        readStreamAsString(response, function(error, content) {
+            if (error) throw error;
+            console.log("Type " + type + ": " + content);
+        });
+    }).end();
+});
+
+
+
+function urlToPath(url) {
+    var path = require("url").parse(url).pathname;
+    var decoded = decodeURIComponent(path);
+    return "." + decoded.replace(/(\/|\\)\.\.(\/|\\|$)/g, "/");
+}
+
+methods.MKCOL = function(path, respond) {
+    fs.stat(path, function(error, stats) {
+        if (error && error.code == "ENOENT")
+            fs.mkdir(path, respondErrorOrNothing(respond));
+        else if (error)
+            respond(500, error.toString());
+        else if (stats.isDirectory())
+            respond(204);
+        else
+            respond(400, "File exists");
+    });
+};
+
 
 
